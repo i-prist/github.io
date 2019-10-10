91a4823fb190764c21b765305c41a833bbafc246
i-pist <i-prist@list.ru>
2019-10-10 15:22:31 +0700

    куенкерк
    кенке

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ function takeGift() {
     const btnGift = document.getElementById('gift');
     const modalGift = document.querySelector('.modal-gift');
     const closeBtn = document.querySelector('.modal-gift__close');
+    const shadow = document.getElementById('shadow');
 
     btnGift.addEventListener('click', () => {
+        shadow.style.display = 'flex';
         modalGift.style.display = 'flex';
         document.body.style.overflow = 'hidden';
     });
 
     closeBtn.addEventListener('click', () => {
         modalGift.style.display = 'none';
+        shadow.style.display = 'none';
         document.body.style.overflow = '';
     });
 }
@@ -20,19 +23,21 @@ function thx() {
     const modalThx = document.querySelector('.modal-thx');
     const closeBtn = document.querySelector('.modal-thx__close');
     const endBtn = document.getElementById('end');
+    const shadow = document.getElementById('shadow');
 
     btnTake.addEventListener('click', () => {
         modalGift.style.display = 'none';
         modalThx.style.display = 'flex';
-        document.body.style.overflow = 'hidden';
     });
 
     closeBtn.addEventListener('click', () => {
         modalThx.style.display = 'none';
+        shadow.style.display = 'none';
         document.body.style.overflow = '';
     });
     endBtn.addEventListener('click', () => {
         modalThx.style.display = 'none';
+        shadow.style.display = 'none';
         document.body.style.overflow = '';
     });
 }
