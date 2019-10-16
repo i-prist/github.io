fa5761d92bd0b829284402aa8ece8f1cf2166ae1
i-pist <i-prist@list.ru>
2019-10-16 22:51:19 +0700

    реккер
    кек

--- src/index.js
+++ src/index.js
@@ -1,43 +1,119 @@
 'use strict';
 
+var ymaps;
+
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
             center: [55.751574, 37.573856],
-            zoom: 9,
-            controls: []
+            zoom: 11,
+            controls: ['zoomControl']
         }, {
             searchControlProvider: 'yandex#search'
         }),
 
         myPlacemark1 = new ymaps.Placemark(myMap.getCenter(), {
-            balloonContentHeader: '<a href = "#">Рога и копыта</a><br>' +
-            '<span class="description">Сеть кинотеатров</span>',
-        balloonContentBody:
-            '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
-            '<b>Ближайшие сеансы</b> <br/> Сеансов нет.',
-        balloonContentFooter: 'Информация предоставлена:<br/>OOO "Рога и копыта"',
-        hintContent: 'Рога и копыта'
+            balloonContentHeader: '<a href = "#">улица Смоленская набережная 53</a><br>' +
+                '<span class="description">G-Energy сервис<br>Главный офис</span>',
+            balloonContentBody: '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+                '<b>Время работы:</b> <br/> 10:00-19:00 без перерывов.',
+            balloonContentFooter: 'Информация предоставлена:<br/>"ПАО Газпром"',
+            hintContent: 'Смоленская набережная 53'
         }, {
             iconLayout: 'default#image',
             iconImageHref: 'pic/market_service.png',
             iconImageSize: [36, 38],
-            iconImageOffset: [-5, -38]
         }),
 
-        myPlacemark2 = new ymaps.Placemark([55.661574, 37.573856], {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'А эта не менее красивая'
+        myPlacemark2 = new ymaps.Placemark([55.757380, 37.633944], {
+            hintContent: 'Маросейка, 4/2с1',
+            balloonContentHeader: '<a href = "#">улица Маросейка, 4/2с1</a><br>' +
+                '<span class="description">G-Energy сервис</span>',
+            balloonContentBody: '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+                '<b>Время работы:</b> <br/> 10:00-19:00 без перерывов.',
+            balloonContentFooter: 'Информация предоставлена:<br/>ИП Селезнёв"'
         }, {
-           
+            iconLayout: 'default#image',
+            iconImageHref: 'pic/market_service.png',
+            iconImageSize: [36, 38],
+        }),
+
+        myPlacemark3 = new ymaps.Placemark([55.767455, 37.594017], {
+            balloonContentHeader: '<a href = "#">Большая Садовая улица, 14с1</a><br>' +
+                '<span class="description">G-Energy сервис</span>',
+            balloonContentBody: '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+                '<b>Время работы:</b> <br/> 10:00-19:00 без перерывов.',
+            balloonContentFooter: 'Информация предоставлена:<br/>ИП Саботович',
+            hintContent: 'Большая Садовая, 14с1'
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'pic/market_service.png',
+            iconImageSize: [36, 38],
+        }),
+
+        myPlacemark4 = new ymaps.Placemark([55.743585, 37.584563], {
+            balloonContentHeader: '<a href = "#">Глазовский переулок, 9</a><br>' +
+                '<span class="description">G-Energy магазин</span>',
+            balloonContentBody: '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+                '<b>Время работы:</b> <br/> 10:00-19:00 без перерывов.',
+            balloonContentFooter: 'Информация предоставлена:<br/>OOO "Стар-307"',
+            hintContent: 'Глазовский пер., 9'
+        }, {
             iconLayout: 'default#image',
             iconImageHref: 'pic/marker_market.png',
             iconImageSize: [36, 38],
-            iconImageOffset: [-24, -24],
+        }),
+
+        myPlacemark5 = new ymaps.Placemark([55.696006, 37.546604], {
+            balloonContentHeader: '<a href = "#">Молодёжная улица, 6</a><br>' +
+                '<span class="description">G-Energy магазин</span>',
+            balloonContentBody: '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+                '<b>Время работы:</b> <br/> 10:00-19:00 без перерывов.',
+            balloonContentFooter: 'Информация предоставлена:<br/>OOO "Гараж рус"',
+            hintContent: 'Молодёжная, 6'
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'pic/marker_market.png',
+            iconImageSize: [36, 38],
+        }),
+
+        myPlacemark6 = new ymaps.Placemark([55.656705, 37.706394], {
+            balloonContentHeader: '<a href = "#">1-я Курьяновская улица, 6</a><br>' +
+                '<span class="description">G-Energy сервис</span>',
+            balloonContentBody: '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+                '<b>Время работы:</b> <br/> 10:00-19:00 без перерывов.',
+            balloonContentFooter: 'Информация предоставлена:<br/>ИП Антонов"',
+            hintContent: '1-я Курьяновская, 6'
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'pic/market_service.png',
+            iconImageSize: [36, 38],
+        }),
+
+        myPlacemark7 = new ymaps.Placemark([55.795569, 37.643861], {
+            balloonContentHeader: '<a href = "#">2-я Мытищинская улица, 2с14</a><br>' +
+                '<span class="description">G-Energy магазин</span>',
+            balloonContentBody: '<a href="tel:+7-123-456-78-90">+7 (123) 456-78-90</a><br/>' +
+                '<b>Время работы:</b> <br/> 10:00-19:00 без перерывов.',
+            balloonContentFooter: 'Информация предоставлена:<br/>OOO "Экзибит"',
+            hintContent: '2-я Мытищинская, 2с14'
+        }, {
+
+            iconLayout: 'default#image',
+            iconImageHref: 'pic/marker_market.png',
+            iconImageSize: [36, 38],
         });
 
     myMap.geoObjects
         .add(myPlacemark1)
-        .add(myPlacemark2);
+        .add(myPlacemark2)
+        .add(myPlacemark3)
+        .add(myPlacemark4)
+        .add(myPlacemark5)
+        .add(myPlacemark6)
+        .add(myPlacemark7);
+
+        myMap.behaviors.disable('scrollZoom');
+
 });
 
 
