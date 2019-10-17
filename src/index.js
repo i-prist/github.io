4a80904035168b0c43daacc1d4afd59b69232550
i-pist <i-prist@list.ru>
2019-10-18 01:44:49 +0700

    ваыаы
    аываыв

--- src/index.js
+++ src/index.js
@@ -112,10 +112,27 @@ ymaps.ready(function () {
         .add(myPlacemark6)
         .add(myPlacemark7);
 
-        myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('scrollZoom');
 
 });
 
+function close() {
+    const modalThx = document.querySelector('.modal-thx');
+    const closeBtn = document.querySelector('.modal-thx__close');
+    const shadow = document.getElementById('shadow');
+    const endBtn = document.getElementById('end');
+
+    closeBtn.addEventListener('click', () => {
+        modalThx.style.display = 'none';
+        shadow.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+    endBtn.addEventListener('click', () => {
+        modalThx.style.display = 'none';
+        shadow.style.display = 'none';
+        document.body.style.overflow = '';
+    });
+}
 
 function takeGift() {
     const btnGift = document.getElementById('gift');
@@ -137,65 +154,30 @@ function takeGift() {
 
 function validate() {
     const form = document.querySelector('.modal-gift__form');
+    const modalGift = document.querySelector('.modal-gift');
     let inputs = form.querySelectorAll('.modal-gift__input');
-
-    let generateError = function (text) {
-        let error = document.createElement('div');
-        error.className = 'error';
-        error.style.color = 'red';
-        error.innerHTML = text;
-        return error;
-    };
-
-    let removeValidation = function () {
-        let errors = form.querySelectorAll('error');
-
-        for (let i = 0; i < errors.length; i++) {
-            errors[i].remove();
-        }
+    const modalThx = document.querySelector('.modal-thx');
+    
+    let generateSucsess = function () {
+        modalGift.style.display = 'none';
+        modalThx.style.display = 'flex';
     };
 
     let checkInputs = function () {
         for (let i = 0; i < inputs.length; i++) {
-            if (!inputs[i].value) {
-                let error = generateError('Заполните поле');
-                form[i].parentElement.insertBefore(error, inputs[i]);
+            if (!inputs[i].value) {} 
+            else {
+                generateSucsess();
             }
         }
     };
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
-        removeValidation();
         checkInputs();
-    });
-}
-
-function thx() {
-    const modalGift = document.querySelector('.modal-gift');
-    const btnTake = document.getElementById('take-gift');
-    const modalThx = document.querySelector('.modal-thx');
-    const closeBtn = document.querySelector('.modal-thx__close');
-    const endBtn = document.getElementById('end');
-    const shadow = document.getElementById('shadow');
-
-    btnTake.addEventListener('onsubmit', () => {
-        modalGift.style.display = 'none';
-        modalThx.style.display = 'flex';
-    });
-
-    closeBtn.addEventListener('click', () => {
-        modalThx.style.display = 'none';
-        shadow.style.display = 'none';
-        document.body.style.overflow = '';
-    });
-    endBtn.addEventListener('click', () => {
-        modalThx.style.display = 'none';
-        shadow.style.display = 'none';
-        document.body.style.overflow = '';
     });
 }
 
 takeGift();
 validate();
-thx();
+close();
